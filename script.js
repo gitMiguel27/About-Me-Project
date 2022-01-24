0a078e1b1b6cb238cbad7359a545862ef41db825
Miguel Nazario <mnazario35@gmail.com>
2022-01-23 19:57:27 -0500

    Finished Project for now

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ h1.textContent = "All About Miguel";
 
 document.querySelector("body").appendChild(h1);
 
+const navAbout = document.getElementById('navAbout');
+
+navAbout.addEventListener("mouseover", function(e) {
+        alert("This does not work yet :/");
+});
+
+const navFun = document.getElementById('navFun');
+
+navFun.addEventListener("mouseover", function(e) {
+        alert("This does not work yet :/");
+});
+
+const navContact = document.getElementById('navContact');
+
+navContact.addEventListener("mouseover", function(e) {
+        alert("This does not work yet :/");
+});
+
